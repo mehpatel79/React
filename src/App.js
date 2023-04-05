e27d564efff6f0efb7e7c788b5455a057d949b03
mehpatel79 <123248785+mehpatel79@users.noreply.github.com>
2023-04-05 21:53:43 +0530

    Mehul - React

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './App.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const list1 = ["Mehul","Parth","Jay","Ajay","Raj"]
-const list2 = ["Ruchi", "Sneha", "Anjali", "Vibhuti", "Jyot"]
-const list3 = ["Sharan", "Vasava", "Parul","ABC","XYZ"]
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);    
-     this.state = {                           
-      candidateName : list1,
-      id:1
-     }
-     this.changeList = this.changeList.bind(this);
-  }
-
-
-changeList = () => {
-  let num = this.state.id;
-  if(num==1) {
-    return (this.setState({candidateName : list2, id:2}));
-  }
-  else if (num==2) {
-      return (this.setState({candidateName : list3, id:3}));    
-    }
-  else if (num==3){
-      return (this.setState({candidateName : list1, id:1}));  
-    }
-  }
+//import StudentState from './StudentContainer/StudentState'
+import Todo from './Todo';
+import Todo2 from './Todo2';
 
 
 
-  render() {
-    return (
-      <div>
-        <ol>
-          {this.state.candidateName.map((candidate) => {
-            return <li key={candidate}> {candidate}</li>
-          })}
-        </ol>
-        <button onClick={this.changeList}>Change List</button>
-      </div>
-    );
-  }
+function App(){
+
+  //  return <StudentState />;
+  //return <Todo />;
+  return <Todo2 />;
+  
 }
-
-
 
 export default App;
